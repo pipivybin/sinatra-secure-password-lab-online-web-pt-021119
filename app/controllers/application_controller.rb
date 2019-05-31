34ab38ab4477f48cd8d687875f932df0b1679aba
Ming Duan <11203duan@gmail.com>
2019-05-31 01:32:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(username: params[:username], password: params[:password])
-    if user.save && user.password != ""
+    if user.save && user.username != ""
       redirect "/login"
     else redirect "/failure"
     end
